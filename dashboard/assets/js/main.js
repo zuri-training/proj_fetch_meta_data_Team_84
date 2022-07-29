1f99ed3957be06231ee884a1d6abef6c074a0123
olayinka olamijulo <olayinkaolamijuloo@gmail.com>
2022-07-29 12:26:56 +0100

    Drag and drop for file upload

--- dashboard/assets/js/main.js
+++ dashboard/assets/js/main.js
@@ -1,74 +1,81 @@
-document.querySelectorAll(".drop-zone__input").forEach(inputElement => {
+document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
     const dropZoneElement = inputElement.closest(".drop-zone");
-
-    dropZoneElement.addEventListener("click", e => {
-        inputElement.click();
+  
+    dropZoneElement.addEventListener("click", (e) => {
+      inputElement.click();
     });
-    
-    inputElement.addEventListener("change", e => {
-        if (inputElement.files.length) {
-            updateThumbnail(dropZoneElement, inputElement.files[0]);
-        }
+  
+    inputElement.addEventListener("change", (e) => {
+      if (inputElement.files.length) {
+        updateThumbnail(dropZoneElement, inputElement.files[0]);
+      }
     });
-
-    dropZoneElement.addEventListener("dragover", e => {
-        e.preventDefault();
-        dropZoneElement.classList.add("drop-zone--over");
+  
+    dropZoneElement.addEventListener("dragover", (e) => {
+      e.preventDefault();
+      dropZoneElement.classList.add("drop-zone--over");
     });
-
-    ["dragleave", "dragend"].forEach(type => {
-        dropZoneElement.addEventListener(type, e => {
-            dropZoneElement.classList.remove("drop-zone--over");
-        });
-    });
-
-    dropZoneElement.addEventListener("drop", e => {
-        e.preventDefault();
-        // console.log(e.dataTransfer.files);
-        if  (e.dataTransfer.files.length) {
-            inputElement.files = e.dataTransfer.files;
-            // console.log(inputElement.files); select multiple files
-            updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
-        }
-
+  
+    ["dragleave", "dragend"].forEach((type) => {
+      dropZoneElement.addEventListener(type, (e) => {
         dropZoneElement.classList.remove("drop-zone--over");
+      });
     });
-});
-
-
-/** 
-*Updates the thumbnail on drop zone element
-*@param {HTMLElement} dropZoneElement
-*@param {File} file
-*/
-
-
-function updateThumbnail(dropZoneElement, file) {
+  
+    dropZoneElement.addEventListener("drop", (e) => {
+      e.preventDefault();
+  
+      if (e.dataTransfer.files.length) {
+        inputElement.files = e.dataTransfer.files;
+        updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+      }
+  
+      dropZoneElement.classList.remove("drop-zone--over");
+    });
+  });
+  
+  /**
+   * Updates the thumbnail on a drop zone element.
+   *
+   * @param {HTMLElement} dropZoneElement
+   * @param {File} file
+   */
+  function updateThumbnail(dropZoneElement, file) {
     let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
-
-    // First time - remove the prompt 
-    if (dropZoneElement.querySelector(".drop-zone__prompt")) {
-        dropZoneElement.querySelector(".drop-zone__prompt").remove();
+  
+    // First time - remove the prompt
+    // if (dropZoneElement.querySelector(".drop-zone__prompt")) {
+    //   dropZoneElement.querySelector(".drop-zone__prompt").remove();
+    // }
+    if (dropZoneElement.querySelector(".top")) {
+        dropZoneElement.querySelector(".top").remove();
     }
-
+    if (dropZoneElement.querySelector(".middle")) {
+        dropZoneElement.querySelector(".middle").remove();
+    }
+      if (dropZoneElement.querySelector(".bottom")) {
+        dropZoneElement.querySelector(".bottom").remove();
+    }
+  
     // First time - there is no thumbnail element, so lets create it
     if (!thumbnailElement) {
-        thumbnailElement = document.createElement("div");
-        thumbnailElement.classList.add("drop-zone__thumb");
-        dropZoneElement.appendChild(thumbnailElement);
+      thumbnailElement = document.createElement("div");
+      thumbnailElement.classList.add("drop-zone__thumb");
+      dropZoneElement.appendChild(thumbnailElement);
     }
-
+  
     thumbnailElement.dataset.label = file.name;
-
+  
     // Show thumbnail for image files
     if (file.type.startsWith("image/")) {
-        const reader = new FileReader();
-
-        reader.readAsDataURL(file);
-        reader.onload = () => {
-            thumbnailElement.style.backgroundImage = `url('${ reader.result }')`;
-        };
+      const reader = new FileReader();
+  
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
+      };
     } else {
-        thumbnailElement.style.backgroundImage = null;
+      thumbnailElement.style.backgroundImage = null;
     }
-}
+  }
+  
