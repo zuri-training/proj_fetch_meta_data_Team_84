47d39f8cb1ed2c893070feac389404e2e93134ce
Adesina Aladesae <101404177+adesinna@users.noreply.github.com>
2022-08-06 15:11:30 +0100

    Add files via upload
    Just Starter Files

---
+++ meta/core/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.views.generic import TemplateView
+from . import views
+from django.contrib.auth.views import LogoutView
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('register/', views.RegisterPage.as_view(), name='register'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='core:login'), name='logout'),
+    path('core/', views.Core.as_view(), name='core')
+
+]
