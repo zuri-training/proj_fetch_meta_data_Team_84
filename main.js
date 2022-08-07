c1f50b6d2c4cd2e5b72e67fc2132e20ea9daf8ab
olayinka olamijulo <olayinkaolamijuloo@gmail.com>
2022-08-07 01:40:42 +0100

    Slide sidebar completed

--- main.js
+++ main.js
@@ -108,3 +108,33 @@ window.addEventListener(
     },
     false
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+{
+  let sideBar = document.querySelector('.side-bar');
+  let arrowCollapse = document.querySelector('#logo-name__icon');
+  sideBar.onclick = () => {
+    sideBar.classList.toggle('collapse');
+    arrowCollapse.classList.toggle('collapse');
+    if (arrowCollapse.classList.contains('collapse')) {
+      arrowCollapse.classList =
+        'fa-solid fa-angle-left logo-name__icon collapse';
+        // 'bx bx-arrow-from-left logo-name__icon collapse';
+    } else {
+      arrowCollapse.classList = 'fa-solid fa-angle-right logo-name__icon';
+      // arrowCollapse.classList = 'bx bx-arrow-from-right logo-name__icon';
+    }
+  };
+}
+
