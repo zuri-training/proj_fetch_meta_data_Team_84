5d3a3dd732cbab3eacf63f94e2d359be66a263d6
CreativeLit <samuelgodson55@gmail.com>
2022-08-09 04:19:04 +0100

    Done with the Real Sign Up Syn

---
+++ fetch_meta_data/static/js/register.js
@@ -0,0 +1,5 @@
+const username = document.querySelector("#username");
+
+username.addEventListener("keyup", () => {
+    console.log('77777777', 77777777)
+});
--- mysite/mysite/info.py
+++
@@ -1,6 +0,0 @@
-EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.mailtrap.io'
-EMAIL_HOST_USER = '17db5d440424f7'
-EMAIL_HOST_PASSWORD = '0bde29005ac0b8'
-EMAIL_PORT = 2525
